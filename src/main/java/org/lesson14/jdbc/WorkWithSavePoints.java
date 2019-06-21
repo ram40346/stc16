f2fca89572065c4cb33329b2d39f4e2bf55b15f2
ram40346 <ramil140960@mail.ru>
2019-06-21 12:13:42 +0300

    Доработан класс WorkWithSavePoints

--- src/main/java/org/lesson14/jdbc/WorkWithSavePoints.java
+++ src/main/java/org/lesson14/jdbc/WorkWithSavePoints.java
@@ -2,14 +2,10 @@ package org.lesson14.jdbc;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomUtils;
-import org.lesson14.tablecontents.Role;
+import org.lesson14.tablecontents.User;
 
 import java.sql.*;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
-
-import static org.lesson14.jdbc.UpDate.*;
 
 /**
  * Работа с транзакциями
@@ -22,54 +18,33 @@ public class WorkWithSavePoints {
         connection.setAutoCommit(false);
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите " + USER_ID);
-        String id = scanner.nextLine();
-        System.out.println("Введите " + USER_NAME);
-        String name = scanner.nextLine();
-        System.out.println("Введите " + USER_EMAIL);
-        String email = scanner.nextLine();
-        System.out.println("Введите " + USER_BIRTHDAY + " в формате yyyy-mm-dd");
-        String birthday = scanner.nextLine();
-        System.out.println("Введите " + USER_LOGIN_ID);
-        String loginId = scanner.nextLine();
-        System.out.println("Введите " + USER_CITY);
-        String city = scanner.nextLine();
-        System.out.println("Введите " + USER_DESCRIPTION);
-        String description = scanner.nextLine();
-        System.out.println("Введите роль");
-        String roleName = scanner.nextLine();
-        DateTimeFormatter dateFormat = DateTimeFormatter.ISO_LOCAL_DATE;
-        LocalDate startDate = LocalDate.parse(birthday, dateFormat);
+        UserScanner.scanUserValues(scanner);
+        User user = UserScanner.scanUserValues(scanner);
+
         Savepoint savepoint = connection.setSavepoint();
 
-        try {
-        String insertSQL = "INSERT INTO public.user_role(id, user_id, role_id) "
-                + "VALUES (?,?,?)";
-        PreparedStatement userRoleStatement = connection.prepareStatement(insertSQL);
-        userRoleStatement.setInt(1, RandomUtils.nextInt());
-        userRoleStatement.setInt(2, Integer.valueOf(id));
-        userRoleStatement.setInt(3, Role.findByName(roleName).getId());
-        userRoleStatement.executeUpdate();
+        try (PreparedStatement userRoleStatement = connection.prepareStatement("INSERT INTO public.user_role(id, user_id, role_id) "
+                + "VALUES (?,?,?)"); PreparedStatement userStatement = connection.prepareStatement("INSERT INTO public.user(id, name, birthday, login_id, city, email, description) "
+                + "VALUES (?,?,?,?,?,?,?)")) {
+            userRoleStatement.setInt(1, RandomUtils.nextInt());
+            userRoleStatement.setInt(2, user.getId());
+            userRoleStatement.setInt(3, user.getRole().getId());
+            userRoleStatement.executeUpdate();
 
-
-            String insertUserSQL = "INSERT INTO public.user(id, name, birthday, login_id, city, email, description) "
-                    + "VALUES (?,?,?,?,?,?,?)";
-            PreparedStatement userStatement = connection.prepareStatement(insertUserSQL);
-            userStatement.setInt(1, Integer.valueOf(id));
-            userStatement.setString(2, name);
-            userStatement.setDate(3, Date.valueOf(birthday));
-            userStatement.setInt(4, Integer.valueOf(loginId));
-            userStatement.setString(5, city);
-            userStatement.setString(6, email);
-            userStatement.setString(7, description);
+            userStatement.setInt(1, user.getId());
+            userStatement.setString(2, user.getName());
+            userStatement.setDate(3, Date.valueOf(user.getBirthday()));
+            userStatement.setInt(4, user.getLoginId());
+            userStatement.setString(5, user.getCity());
+            userStatement.setString(6, user.getEmail());
+            userStatement.setString(7, user.getDescription());
             userStatement.executeUpdate();
             connection.commit();
 
         } catch (SQLException | NumberFormatException | NullPointerException e) {
             connection.rollback(savepoint);
             log.error(e.getMessage());
-        } finally {
-            connection.close();
+
         }
     }
 }
